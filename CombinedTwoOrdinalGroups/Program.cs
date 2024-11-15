94504ff7128d4a10c3e3879402bef6ebbe4a881e
HackerQWERT <HackerQWERT@outlook.com>
2024-11-15 17:36:37 +0800

    Init

---
+++ CombinedTwoOrdinalGroups/Program.cs
@@ -0,0 +1,27 @@
+﻿Solution solution = new Solution();
+int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+int[] nums2 = new int[] { 2, 5, 6 };
+solution.Merge(nums1, 3, nums2, 3);
+
+public class Solution
+{
+
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+
+
+
+
+
+
+
+
+
+
+        //循环打印Nums1
+        for (int i = 0; i < m; i++)
+        {
+            Console.WriteLine(nums1[i]);
+        }
+    }
+}
